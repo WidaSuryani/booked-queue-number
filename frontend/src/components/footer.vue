79833562b167362a8f4eb215a98d6ec39e871dd6
WidaSuryani <widasuryani1@gmail.com>
2025-03-05 16:54:58 +0800

    feat : add footer

---
+++ frontend/src/components/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer
+    class="bg-linear-to-r from-[#9bc6f3] to-[#FFF] rounded-lg shadow-sm m-4"
+  >
+    <div
+      class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
+    >
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+        >© 2025
+        <a
+          href="https://www.linkedin.com/in/wida-suryani-60a3a5222/"
+          class="hover:underline"
+          >WidaSuryani</a
+        >. All Rights Reserved.
+      </span>
+      <ul
+        class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+      >
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+        </li>
+        <li>
+          <a href="#" class="hover:underline">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
